d578cb5e2afb8b2abda4abec68f79707678f3da7
Hezel2000 <86461739+Hezel2000@users.noreply.github.com>
2024-01-24 11:41:22 +0100

    test

--- pages/01 Browse Datasets.py
+++ pages/01 Browse Datasets.py
@@ -58,6 +58,7 @@ def get_metadata(repo_owner, repo_name, folder, file_name):
         return f"Error: Unable to fetch files. Status code: {response.status_code}"
 tmp = get_metadata("Hezel2000", "mag4datasets", "metadata", "Banda Arc")
 st.write(tmp)
+st.write(test)
 
 @st.cache_data
 def get_csv_urls(repo_owner, repo_name, folder):
