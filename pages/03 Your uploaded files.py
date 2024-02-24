828de12ef3e07c2e88ec5aca93917d62826a81c5
Dominik <dominik.hezel@em.uni-frankfurt.de>
2024-02-24 13:39:56 +0100

    test

--- pages/03 Your uploaded files.py
+++ pages/03 Your uploaded files.py
@@ -11,6 +11,7 @@ if st.session_state.is_authenticated == True:
     df_metadata_personal.insert(0, 'delete', False)
     editable_columns = ['update', 'delete']
     columns_not_to_be_edited = [item for item in df_metadata_personal.columns.tolist() if item not in editable_columns]
+    st.write(columns_not_to_be_edited)
     st.data_editor(df_metadata_personal, disabled = columns_not_to_be_edited, hide_index=True)
 
 else:
