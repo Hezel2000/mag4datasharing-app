52b4b0a984e323447556f870d063adac2c0c603a
Dominik <dominik.hezel@em.uni-frankfurt.de>
2024-02-24 13:21:40 +0100

    test

--- pages/03 Your uploaded files.py
+++ pages/03 Your uploaded files.py
@@ -5,16 +5,16 @@ st.title('Your uploaded datasets')
 
 if st.session_state.is_authenticated == True:
     df_metadata = pd.read_csv('https://raw.githubusercontent.com/Hezel2000/mag4datasets/main/overview_available_datasets.csv')
-    st.dataframe(df_metadata[df_metadata['ORCID'] == st.session_state.orcid_user_info['id'].split('/')[-1]])
+    df_metadata_personal = st.dataframe(df_metadata[df_metadata['ORCID'] == st.session_state.orcid_user_info['id'].split('/')[-1]])
 
     checkbox_col1 = st.checkbox("Checkbox 1", key='checkbox1')
     checkbox_col2 = st.checkbox("Checkbox 2", key='checkbox2')
 
     # Concatenate the checkboxes with the DataFrame
-    df_with_checkboxes = pd.concat([pd.Series([checkbox_col1] * len(df_metadata), name="update"),
-                                    pd.Series([checkbox_col2] * len(df_metadata), name="delete"),
-                                    df_metadata], axis=1)
-    st.write(df_with_checkboxes, index=False)
+    df_with_checkboxes = pd.concat([pd.Series([checkbox_col1] * len(df_metadata_personal), name="update"),
+                                    pd.Series([checkbox_col2] * len(df_metadata_personal), name="delete"),
+                                    df_metadata_personal], axis=1)
+    st.dataframe(df_with_checkboxes)
 
 else:
     st.subheader('Authenticate with ORCID to see your uploaded datsets.')
