0ab35cb8ea5589d5bf9b6bbd8f88278f1759ab5d
Dominik <dominik.hezel@em.uni-frankfurt.de>
2024-02-24 13:34:26 +0100

    test

--- pages/03 Your uploaded files.py
+++ pages/03 Your uploaded files.py
@@ -7,13 +7,14 @@ if st.session_state.is_authenticated == True:
     df_metadata = pd.read_csv('https://raw.githubusercontent.com/Hezel2000/mag4datasets/main/overview_available_datasets.csv')
     df_metadata_personal = df_metadata[df_metadata['ORCID'] == st.session_state.orcid_user_info['id'].split('/')[-1]]
 
-    checkbox_col1 = st.checkbox("Checkbox 1", key='checkbox1')
-    checkbox_col2 = st.checkbox("Checkbox 2", key='checkbox2')
+    # checkbox_col1 = st.checkbox("Checkbox 1", key='checkbox1')
+    # checkbox_col2 = st.checkbox("Checkbox 2", key='checkbox2')
 
-    # Concatenate the checkboxes with the DataFrame
-    df_with_checkboxes = pd.concat([pd.Series(True * len(df_metadata_personal), name="update"),
-                                    pd.Series([checkbox_col2] * len(df_metadata_personal), name="delete"),
-                                    df_metadata_personal], axis=1)
+    # # Concatenate the checkboxes with the DataFrame
+    # df_with_checkboxes = pd.concat([pd.Series(True * len(df_metadata_personal), name="update"),
+    #                                 pd.Series([checkbox_col2] * len(df_metadata_personal), name="delete"),
+    #                                 df_metadata_personal], axis=1)
+    df_with_checkboxes.insert(0, 'update', True)
     st.data_editor(df_with_checkboxes, disabled = ['Short Title', 'Licence'], hide_index=True)
 
 else:
