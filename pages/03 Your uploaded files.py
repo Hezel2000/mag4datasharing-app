598edf43293184efdfacfca1d6604ddbec9cd7b3
Dominik <dominik.hezel@em.uni-frankfurt.de>
2024-02-24 13:39:18 +0100

    test

--- pages/03 Your uploaded files.py
+++ pages/03 Your uploaded files.py
@@ -9,7 +9,9 @@ if st.session_state.is_authenticated == True:
 
     df_metadata_personal.insert(0, 'update', False)
     df_metadata_personal.insert(0, 'delete', False)
-    st.data_editor(df_metadata_personal, disabled = df_metadata_personal.columns.tolist(), hide_index=True)
+    editable_columns = ['update', 'delete']
+    columns_not_to_be_edited = [item for item in df_metadata_personal.columns.tolist() if item not in editable_columns]
+    st.data_editor(df_metadata_personal, disabled = columns_not_to_be_edited, hide_index=True)
 
 else:
     st.subheader('Authenticate with ORCID to see your uploaded datsets.')
