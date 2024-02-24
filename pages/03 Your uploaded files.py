43e53cb127e3c3afaa870cec9529824eeb7710a0
Dominik <dominik.hezel@em.uni-frankfurt.de>
2024-02-24 13:09:23 +0100

    test

--- pages/03 Your uploaded files.py
+++ pages/03 Your uploaded files.py
@@ -1,6 +1,7 @@
 import streamlit as st
 
 st.title('Your uploaded files')
+st.write(st.session_state.orcid_user_info['id'])
 if st.session_state.is_authenticated == True:
     df_metadata = pd.read_csv('https://raw.githubusercontent.com/Hezel2000/mag4datasets/main/overview_available_datasets.csv')
     st.dataframe(df_metadata[df_metadata['ORCID'] == st.session_state.orcid_user_info['id']])
