f16ad660ecfdfdf6376a904e0f8e923c9b23d535
Dominik <dominik.hezel@em.uni-frankfurt.de>
2024-02-24 13:14:29 +0100

    checkboxes

--- pages/03 Your uploaded files.py
+++ pages/03 Your uploaded files.py
@@ -1,12 +1,21 @@
 import streamlit as st
 import pandas as pd
 
-st.title('Your uploaded files')
-st.write(st.session_state.orcid_user_info['id'].split('/')[-1])
+st.title('Your uploaded datasets')
 
 if st.session_state.is_authenticated == True:
     df_metadata = pd.read_csv('https://raw.githubusercontent.com/Hezel2000/mag4datasets/main/overview_available_datasets.csv')
     st.dataframe(df_metadata[df_metadata['ORCID'] == st.session_state.orcid_user_info['id'].split('/')[-1]])
+
+    checkbox_col1 = st.checkbox("Checkbox 1", key='checkbox1')
+    checkbox_col2 = st.checkbox("Checkbox 2", key='checkbox2')
+
+    # Concatenate the checkboxes with the DataFrame
+    df_with_checkboxes = pd.concat([pd.Series([checkbox_col1] * len(df), name="Checkbox 1"),
+                                    pd.Series([checkbox_col2] * len(df), name="Checkbox 2"),
+                                    df_metadata], axis=1)
+    st.dataframe(df_with_checkboxes)
+
 else:
     st.subheader('Authenticate with ORCID to see your uploaded datsets.')
 
