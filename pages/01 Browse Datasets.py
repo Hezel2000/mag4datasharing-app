f2b0c13de4a17e83fd1b5dcb3a64190f2b7fb4c9
Dominik <dominik.hezel@em.uni-frankfurt.de>
2024-02-24 13:54:53 +0100

    test

--- pages/01 Browse Datasets.py
+++ pages/01 Browse Datasets.py
@@ -44,7 +44,7 @@ with tab1:
 
 with tab2:
     file_urls = get_csv_urls("Hezel2000", "mag4datasets", "data")
-    sel_dataset = st.selectbox('sel', df_metadata['Title'].sort_values(), placeholder='click to select a dataset', label_visibility='collapsed')
+    sel_dataset = st.selectbox('sel', df_metadata['Title'].sort_values(), index=None, placeholder='click to select a dataset', label_visibility='collapsed')
 
     if sel_dataset == None:
         st.write('')
